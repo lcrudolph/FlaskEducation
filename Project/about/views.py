c854fa9777fa78ce1afc0a689a57eb2b3c8c964b
lcrudolph <33425086+lcrudolph@users.noreply.github.com>
2022-03-06 19:13:43 +0000

    fixed the about default page

--- Project/about/views.py
+++ Project/about/views.py
@@ -6,4 +6,10 @@ about_blueprint = Blueprint('about', __name__,template_folder='templates')
 
 @about_blueprint.route("/info")
 def info():
+    """Testing a method that what is shown"""
+    return render_template('info.html')
+    
+@about_blueprint.route("/")
+def info2():
+    """Testing a method that what is shown"""
     return render_template('info.html')
